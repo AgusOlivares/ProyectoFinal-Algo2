5ff8e0c73174afc781d80ef85a5d129c7140d00d
Victor Ramirez <victorriver6@hotmail.com>
2023-05-12 14:55:02 -0300

    Primera version Serializacion

--- code/uber.py
+++ code/uber.py
@@ -1,7 +1,43 @@
 import mapa as m
 
-E = [1 , 2 , 3 , 4]
-A = [(1 , 2 ,30) , (2 , 3 ,70) , (4 , 1 ,10) , (2 , 4 ,20)]
-mapa = m.Graph(E , A)
-new_map = mapa.createGraph(E , A)
-print("X")
+def leer_archivo(archivo):
+    V = []
+    A = []
+    """
+    line.split("=") separa la lista en 2 y luego elejimos el segundo elemento [1]
+    luego la funcion eval() toma esa linea como una linea valida de python y 
+    le asigna esa lista a la variable lista_e y lista_a respectivamente
+    """    
+    with open(archivo, "r") as file:
+        for line in file:
+            if line.startswith("E"):
+                # Extraer la lista de E
+                V = eval(line.split("=")[1])
+            elif line.startswith("A"):
+                # Extraer la lista de A
+                A = eval(line.split("=")[1])
+
+    return V, A
+
+def serializar_archivo(objeto):
+    import pickle
+
+    with open("serializado.txt" , "bw") as ser:
+        pickle.dump(objeto , ser)
+
+def hacer_lectura(archivo):
+    import pickle   
+
+    with open(archivo , "br") as map:
+        mapa = pickle.load(map)
+    return mapa
+
+V, A = leer_archivo("mapa.txt")
+print(V)
+print(A)
+mapa = m.Graph(V , A)
+new_map = mapa.createGraph(V , A)
+serializar_archivo(new_map)
+aux = hacer_lectura("serializado.txt")
+
+
