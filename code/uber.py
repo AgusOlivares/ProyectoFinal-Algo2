30de6c5ece356bbf386b5343d30ccb8d2427b35d
Agustin Olivares <Faolivares3112@gmail.com>
2023-06-05 11:23:10 -0300

    Primera version ejecucion desde terminal

--- code/uber.py
+++ code/uber.py
@@ -1,4 +1,60 @@
 import mapa as m
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-create_map', type=str, help='Carga mapa desde archivo local')
+args = parser.parse_args()
+
+
+if args.create_map:
+
+    def leer_archivo(archivo):
+        V = []
+        A = []
+        """
+        line.split("=") separa la lista en 2 y luego elejimos el segundo elemento [1]
+        luego la funcion eval() toma esa linea como una linea valida de python y 
+        le asigna esa lista a la variable lista_e y lista_a respectivamente
+        """    
+        with open(archivo, "r") as file:
+            for line in file:
+                if line.startswith("E"):
+                    # Extraer la lista de E
+                    V = eval(line.split("=")[1])
+                elif line.startswith("A"):
+                    # Extraer la lista de A
+                    A = eval(line.split("=")[1])
+
+        return V, A
+
+    def serializar_archivo(objeto):  # Se serializa el mapa (Diccionario)
+        import pickle
+
+        with open("serializado.txt" , "bw") as ser:
+            pickle.dump(objeto , ser)
+
+    def hacer_lectura(archivo):
+        import pickle   
+
+        with open(archivo , "br") as map:
+            mapa = pickle.load(map)
+        return mapa
+
+
+    V, A = leer_archivo(args.load_map)
+    mapa = m.Map(V , A)
+    new_map = mapa.createMap(V , A) # Mapa con nodos y aristas
+    print(new_map)
+    
+    serializar_archivo(new_map)
+    print(V)
+
+
+
+
+
+'''
 
 def leer_archivo(archivo):
     V = []
@@ -33,15 +89,17 @@ def hacer_lectura(archivo):
     return mapa
 
 
-# def load_map(local_path): ## Planteamiento
-#     V, A = leer_archivo(local_path)
-#     mapa = m.Map(V , A)
-#     new_map = mapa.createMap(V , A) # Mapa con nodos y aristas
-#     serializar_archivo(new_map)
-#     print(V)
+def load_map(local_path): ## Planteamiento
+     V, A = leer_archivo(local_path)
+     mapa = m.Map(V , A)
+     new_map = mapa.createMap(V , A) # Mapa con nodos y aristas
+     serializar_archivo(new_map)
+     print(V)
 
+'''
 #def load_fix_element(nombre, direccion, map=hacer_lectura("serializado.txt")): ## Planteamiento
 
+"""
 V, A = leer_archivo("mapa.txt")
 print(V)
 print(A)
@@ -59,4 +117,5 @@ print(mapa.doble_sentido(new_map , "P1"))
 
 print("hola")
 
+"""
 
